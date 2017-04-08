5151a271326edf32f5d0cb708ff196b8b11edf07
Magdi Zamel <MSZ@Magdis-MacBook-Air.local>
2017-04-08 03:57:22 +0200

    intial

---
+++ session 1.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+typealias Audio = UInt16
+
+let x = ("sdfdsf",4)
+
+var r :Int = 5
+var t = r
+r = 4
+print(t)
+struct eee{
+
+    static func oo(){
+    print("das")
+    }
+}
+
+eee.oo()
+
+guard <#condition#> else {
+    <#statements#>
+}
+
+guard  t==7 else {
+    
+   print("dasdsa")
+   return
+}
